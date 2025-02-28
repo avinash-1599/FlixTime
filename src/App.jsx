838ad8657a1951082773173e2ebb041f9741405c
Avinash Kumar <avinashkumar@Avinashs-MacBook-Air.local>
2025-02-28 22:54:45 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+
+  return (
+      <div className='font-bold text-2xl text-blue-400'>
+        Lets build FlixTime using React and Vite
+      </div>
+  )
+}
+
+export default App
